30a7af86e60495a9a6378784de034edaf5462624
Amal Jos <amal.jos3@gmail.com>
2021-01-07 23:13:54 +0530

    refs #1 SpringBoot project structure

---
+++ src/main/java/com/portfolio/backend/PortfolioArticleCrudApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioArticleCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioArticleCrudApplication.class, args);
+	}
+
+}
